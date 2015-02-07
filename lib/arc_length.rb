28f2cf3c54efab684a6e538207579abd3dd57e5c
errodr <eric@ericr.com>
2015-02-07 09:57:49 -0600

    Added a type check and made circumference private

--- lib/arc_length.rb
+++ lib/arc_length.rb
@@ -2,16 +2,23 @@ require_relative 'arc_length/version'
 
 module ArcLength
   class Circle
+
     def initialize(radius)
       @radius = radius
     end
 
+    def calculate_arc_length(arc_measure)
+      begin
+        circumference * (arc_measure.to_f/360)
+      end
+    rescue TypeError
+      raise ArgumentError
+    end
+
+    private
     def circumference
       2 * Math::PI * @radius
     end
 
-    def calculate_arc_length(arc_measure)
-      circumference * (arc_measure.to_f/360)
-    end
   end
 end
